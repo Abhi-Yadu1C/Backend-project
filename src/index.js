6b59cd99e74cb76e82e2bf372d3d4c190ce5ee4f
Abhishek Yadav <anuragabhi7303@gmail.com>
2024-09-14 17:30:30 +0530

    Handling response and error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import express from "express";
+import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 import dotenv from "dotenv"
 
 dotenv.config()
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.error(`MONGODB connection failed !!!`, err)
+})
